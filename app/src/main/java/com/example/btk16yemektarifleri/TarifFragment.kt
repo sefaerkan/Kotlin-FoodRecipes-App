d2ad683ac85f378c3f9ae0034c5093b7fd267e5e
Sefa Erkan <erkansefa138@gmail.com>
2023-04-05 02:23:44 +0300

    Yemek Tarifleri

---
+++ app/src/main/java/com/example/btk16yemektarifleri/TarifFragment.kt
@@ -0,0 +1,124 @@
+package com.example.btk16yemektarifleri
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.fragment_tarif.*
+import java.lang.Exception
+
+class TarifFragment : Fragment() {
+
+    var secilenGorsel : Uri? = null
+    var secilenBitmap : Bitmap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tarif, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            kaydet(it)
+        }
+
+        imageView.setOnClickListener {
+            gorselSec(it)
+        }
+
+
+    }
+
+    fun kaydet(view: View){
+        //sql kaydetme
+    }
+
+    fun gorselSec(view: View){
+
+        activity?.let {
+            if (ContextCompat.checkSelfPermission(it.applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                //izin verilmedi, izin istememiz gerekiyor
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            } else {
+                //izin zaten verilmiş, tekrar istemeden galeriye git
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+            }
+        }
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        if (requestCode == 1){
+
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //izni aldık
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+
+            }
+
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if(requestCode == 2 && resultCode == Activity.RESULT_OK && data != null){
+
+            secilenGorsel = data.data
+
+            try {
+
+                context?.let {
+                    if(secilenGorsel != null) {
+                        if( Build.VERSION.SDK_INT >= 28){
+                            val source = ImageDecoder.createSource(it.contentResolver,secilenGorsel!!)
+                            secilenBitmap = ImageDecoder.decodeBitmap(source)
+                            imageView.setImageBitmap(secilenBitmap)
+                        } else {
+                            secilenBitmap = MediaStore.Images.Media.getBitmap(it.contentResolver,secilenGorsel)
+                            imageView.setImageBitmap(secilenBitmap)
+                        }
+
+                    }
+                }
+
+
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+}
